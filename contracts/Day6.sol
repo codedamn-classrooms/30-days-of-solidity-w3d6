f31395d55e030e8900632cebe2716a7b9e93734d
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-04-26 11:11:22 +0530

    Update Day6.sol

--- contracts/Day6.sol
+++ contracts/Day6.sol
@@ -2,15 +2,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Day6 {
-    function sumarray(int256[] memory arr, uint256 size)
-        public
-        pure
-        returns (int256)
-    {
-        int256 sum = 0;
-        for (uint256 i = 0; i < size; i++) {
-            sum += arr[i];
-        }
-        return sum;
-    }
+   
 }
